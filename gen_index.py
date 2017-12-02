d7584090d20681ce7a840bd0525c7f26c67c047c
Nipunn Koorapati <nipunn@dropbox.com>
2017-12-02 13:29:41 -0800

    Use SITE_TEMPLATE

--- gen_index.py
+++ gen_index.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import urllib
 
+SITE_TEMPLATE = """
+<a href={bookmarklet}>Drag This Link to Bookmark Bar to Install</a>
+"""
+
 script = "paper_toggle_rebrand.js"
 with open("paper_toggle_rebrand.js", 'r') as fp:
     contents = urllib.quote(fp.read())
 
 with open("index.html", "w") as fp:
-    fp.write("<a href={}>Drag to Bookmarks to Install</a>".format(contents))
+    fp.write(SITE_TEMPLATE.format(bookmarklet=contents))
