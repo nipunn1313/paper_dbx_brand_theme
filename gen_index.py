e36112e8401a20ae2e965559f9b702c9b4d882a3
Nipunn Koorapati <nipunn@dropbox.com>
2017-12-02 15:47:18 -0800

    Remove javascript: from function

--- gen_index.py
+++ gen_index.py
@@ -9,7 +9,7 @@ SITE_TEMPLATE = """
 <title>nipunn1313/paper_dbx_brand_theme</title>
 </head>
 
-Drag This Link to Bookmark Bar to Install: <a href="{bookmarklet}">Paper: Toggle DBX Theme</a>
+Drag This Link to Bookmark Bar to Install: <a href="javascript:{bookmarklet}">Paper: Toggle DBX Theme</a>
 
 </body>
 </html>
@@ -17,7 +17,7 @@ Drag This Link to Bookmark Bar to Install: <a href="{bookmarklet}">Paper: Toggle
 
 script = "paper_toggle_rebrand.js"
 with open("paper_toggle_rebrand.js", 'r') as fp:
-    contents = urllib.quote(fp.read())
+    contents = urllib.quote(fp.read())[len("javascript:"):]
 
 with open("index.html", "w") as fp:
     fp.write(SITE_TEMPLATE.format(bookmarklet=contents))
